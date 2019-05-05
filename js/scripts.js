1407dee2fd2acc56cc46834157ff68786e32d044
dennis-mwai <dennis.mwai55@gmail.com>
2019-05-06 02:55:37 +0300

    add mouse events using jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,42 @@
 $(document).ready(function (){
-    $("p.card-text").hide();
-    $("img#designimage").hover(function()
-    {
-        $("p#designparagraph").toggle();
+
+    $('#developmentimage').on('click',function(){
+        $('#developmentimage').hide(200);
+        $('#developmenttext').show(200);
+    });
+    $('#developmenttext').on('click',function(){
+        $('#developmenttext').hide(200);
+        $('#developmentimage').show(200);
+    });
+    $('#designimage').on('click',function(){
+        $('#designimage').hide(200);
+        $('#designtext').show(200);
+    });
+    $('#designtext').on('click',function(){
+        $('#designtext').hide(200);
+        $('#designimage').show(200);
+    });
+    $('#productimage').on('click',function(){
+        $('#productimage').hide(200);
+        $('#producttext').show(200);
+    });
+    $('#producttext').on('click',function(){
+        $('#producttext').hide(200);
+        $('#productimage').show(200);
+    })
+
+    $(function(){
+        $('#work4image').on('mouseover',function(){
+            $('#work4text').html('work 4 u');
+            $('#work4image').fadeTo("slow", 0.15);
+        });
+        $('#work4text').on('mouseover',function(){
+            $('#work4image').fadeTo("slow", 1);
+        });
     });
 
-    $("img#developmentimage").hover(function()
-    {
-        $("p#developmenttext").toggle();
-    });
-
-    $("img#productimage").hover(function()
-    {
-        $("p#producttext").toggle();
-    });
 });
+
+
+
+
